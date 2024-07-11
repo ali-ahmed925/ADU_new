cfef3ed38ad5882fd667bf145097a167476596e8
510yuta <yuta2m09s62.5488@outlook.jp>
2024-07-11 14:49:43 +0900

    clip adapterを使えるようにした

---
+++ scripts/clip-adapter/domain_forgetting.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#cd ../..
+export CUDA_VISIBLE_DEVICES=0
+# custom config
+DATA="/nas/data/gotoyuta/Dataset/"
+TRAINER=CLIP_Adapter
+
+DATASET=$1
+CFG=$2  # config file
+# CTP=$3  # class token position (end or middle or front)
+# NCTX=$4  # number of context tokens
+# SHOTS=$5  # number of shots (1, 2, 4, 8, 16)
+# CSC=$5  # class-specific context (False or True)
+
+for SEED in 1
+do
+    DIR=output/${DATASET}/${TRAINER}/${CFG}/seed${SEED}
+    if [ -d "$DIR" ]; then
+        echo "Results are available in ${DIR}. Skip this job"
+    else
+        echo "Run this job and save the output to ${DIR}"
+        python train.py \
+        --root ${DATA} \
+        --seed ${SEED} \
+        --trainer ${TRAINER} \
+        --dataset-config-file configs/datasets/${DATASET}.yaml \
+        --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+        --output-dir ${DIR} \
+        # TRAINER.COOP.N_CTX ${NCTX} \
+        # TRAINER.COOP.CSC ${CSC} \
+        # TRAINER.COOP.CLASS_TOKEN_POSITION ${CTP} \
+        # DATASET.NUM_SHOTS ${SHOTS}
+    fi
+done
---
+++ trainers/clip_adapter.py
@@ -0,0 +1,389 @@
+import os.path as osp
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+from dassl.engine import TRAINER_REGISTRY, TrainerX
+from dassl.metrics import compute_accuracy
+from dassl.utils import load_pretrained_weights, load_checkpoint
+from dassl.optim import build_optimizer, build_lr_scheduler
+
+from clip import clip
+from clip.simple_tokenizer import SimpleTokenizer as _Tokenizer
+
+import datetime
+from utils.loss_fn import EntropyMaximizationLoss
+from utils.eval_acc import compute_acc_for_df, compute_acc_for_df_eval
+from dassl.utils import (
+    MetricMeter, AverageMeter, tolist_if_not, count_num_param, load_checkpoint,
+    save_checkpoint, mkdir_if_missing, resume_from_checkpoint,
+    load_pretrained_weights
+)
+import time
+from tqdm import tqdm
+
+_tokenizer = _Tokenizer()
+
+
+CUSTOM_TEMPLATES = {
+    'OxfordPets': 'a photo of a {}, a type of pet.',
+    'OxfordFlowers': 'a photo of a {}, a type of flower.',
+    'FGVCAircraft': 'a photo of a {}, a type of aircraft.',
+    'DescribableTextures': '{} texture.',
+    'EuroSAT': 'a centered satellite photo of {}.',
+    'StanfordCars': 'a photo of a {}.',
+    'Food101': 'a photo of {}, a type of food.',
+    'SUN397': 'a photo of a {}.',
+    'Caltech101': 'a photo of a {}.',
+    'UCF101': 'a photo of a person doing {}.',
+    'ImageNet': 'a photo of a {}.',
+    'ImageNetSketch': 'a photo of a {}.',
+    'ImageNetV2': 'a photo of a {}.',
+    'ImageNetA': 'a photo of a {}.',
+    'ImageNetR': 'a photo of a {}.',
+    'OfficeHomeDF': 'a photo of a {}.'
+}
+
+
+def load_clip_to_cpu(cfg):
+    backbone_name = cfg.MODEL.BACKBONE.NAME
+    url = clip._MODELS[backbone_name]
+    model_path = clip._download(url)
+    
+    try:
+        # loading JIT archive
+        model = torch.jit.load(model_path, map_location='cpu').eval()
+        state_dict = None
+    
+    except RuntimeError:
+        state_dict = torch.load(model_path, map_location='cpu')
+    
+    design_details = {"trainer": 'CoOp',
+                      "vision_depth": 0,
+                      "language_depth": 0, "vision_ctx": 0,
+                      "language_ctx": 0}
+
+    model = clip.build_model(state_dict or model.state_dict(), design_details)
+
+    return model
+
+
+class Adapter(nn.Module):
+    def __init__(self, c_in, reduction=4):
+        super(Adapter, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(c_in, c_in // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(c_in // reduction, c_in, bias=False),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+    
+    
+class TextEncoder(nn.Module):
+
+    def __init__(self, cfg, classnames, clip_model):
+        super().__init__()
+        self.cfg = cfg
+        self.classnames = classnames
+        self.clip_model = clip_model
+        self.dtype = clip_model.dtype
+    
+    def forward(self):
+        temp = CUSTOM_TEMPLATES[self.cfg.DATASET.NAME]
+        prompts = [temp.format(c.replace('_', ' ')) for c in self.classnames]
+        prompts = torch.cat([clip.tokenize(p) for p in prompts])
+        prompts = prompts.to('cuda')
+        text_features = self.clip_model.encode_text(prompts)
+        x = text_features
+        return x
+
+
+class CustomCLIP(nn.Module):
+
+    def __init__(self, cfg, classnames, clip_model):
+        super().__init__()
+        self.image_encoder = clip_model.visual
+        self.text_encoder = TextEncoder(cfg, classnames, clip_model)
+        self.eos_pro = ()
+        self.logit_scale = clip_model.logit_scale
+        self.dtype = clip_model.dtype
+        self.adapter = Adapter(512, 4).to(clip_model.dtype)
+
+            
+    def forward(self, image):
+        image_features = self.image_encoder(image.type(self.dtype))
+        x = self.adapter(image_features)
+
+        ratio = 0.2
+        image_features = ratio * x + (1 - ratio) * image_features
+
+        text_features = self.text_encoder()
+
+        image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+        text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+
+        logit_scale = self.logit_scale.exp()
+        logits = logit_scale * image_features @ text_features.t()
+
+        return logits
+
+
+@TRAINER_REGISTRY.register()
+class CLIP_Adapter(TrainerX):
+    """ CLIP-Adapter """
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        self.domain_list = ["art", "clipart", "product", "real_world"]
+        self.prv_domain_list = ["clipart", "product", "real_world"]
+        self.del_domain_list = ["art"]
+
+    def build_model(self):
+        cfg = self.cfg
+        classnames = self.dm.dataset.classnames
+
+        print(f'Loading CLIP (backbone: {cfg.MODEL.BACKBONE.NAME})')
+        clip_model = load_clip_to_cpu(cfg)
+        clip_model.float()
+
+        print('Building custom CLIP')
+        self.model = CustomCLIP(cfg, classnames, clip_model)
+
+        print('Turning off gradients in both the image and the text encoder')
+        for name, param in self.model.named_parameters():
+            if 'adapter' not in name:
+                param.requires_grad_(False)
+
+        if cfg.MODEL.INIT_WEIGHTS:
+            load_pretrained_weights(self.model.adapter, cfg.MODEL.INIT_WEIGHTS)
+
+        
+        self.model.to(self.device)
+        # NOTE: only give text_encoder.adapter to the optimizer
+        self.optim = build_optimizer(self.model.adapter, cfg.OPTIM)
+        self.sched = build_lr_scheduler(self.optim, cfg.OPTIM)
+        
+
+        self.register_model('clip_adapter', self.model.adapter, self.optim, self.sched)
+
+        device_count = torch.cuda.device_count()
+        if device_count > 1:
+            print(f'Multiple GPUs detected (n_gpus={device_count}), use all of them!')
+            self.model = nn.DataParallel(self.model)
+
+    def forward_backward(self, batch):
+        image, label, domain = self.parse_batch_train(batch)
+        output = self.model(image)
+        is_DF = True #FIXME
+        if is_DF :
+            entropy_max_loss = EntropyMaximizationLoss()
+            domain_list = ["art", "clipart", "product", "real_world"]
+            prv_domain_list = ["clipart", "product", "real_world"]
+            del_domain_list = ["art"]
+            # domain_list = [
+            #             "clipart", 
+            #             "painting",
+            #             "real",
+            #             "sketch"
+            #             ]
+            # prv_domain_list = [
+            #             "painting",
+            #             "real",
+            #             "sketch"
+            #             ]
+            # del_domain_list = [
+            #             "clipart"
+            #             ]
+
+            # prv_domain_mask = torch.ones_like(domain, dtype=torch.bool)
+            # del_domain_mask = torch.ones_like(domain, dtype=torch.bool)
+            false_check_tensor = torch.zeros_like(domain, dtype=torch.bool)
+            
+            # for prv_domain in prv_domain_list:
+            prv_domain_index = [self.domain_list.index(prv_d) for prv_d in self.prv_domain_list if prv_d in self.domain_list]
+            prv_domain_mask = torch.isin(domain, torch.tensor(prv_domain_index).to(self.device))
+            # for del_domain in del_domain_list:
+            del_domain_index = [self.domain_list.index(del_d) for del_d in self.del_domain_list if del_d in self.domain_list]
+            del_domain_mask = torch.isin(domain, torch.tensor(del_domain_index).to(self.device))
+            
+            if torch.equal(false_check_tensor, prv_domain_mask):
+                loss_prv = 0
+            else :
+                loss_prv = F.cross_entropy(output[prv_domain_mask], label[prv_domain_mask])
+            if torch.equal(false_check_tensor, del_domain_mask):
+                loss_del = 0
+            else :
+                loss_del = entropy_max_loss(output[del_domain_mask])
+            loss = 0.01 * (loss_prv + loss_del)
+        else: 
+            loss = F.cross_entropy(output, label)
+        self.model_backward_and_update(loss)
+
+        loss_summary = {
+            "loss": loss.item(),
+            "loss_prv": loss_prv.item() if isinstance(loss_prv, torch.Tensor) else loss_prv,
+            "loss_del": loss_del.item() if isinstance(loss_del, torch.Tensor) else loss_del,
+            # "acc": compute_accuracy(output, label)[0].item(),
+        }
+        acc = compute_acc_for_df(output, label, prv_domain_mask, del_domain_mask, domain, self.domain_list, device=self.device)
+        loss_summary.update(acc)
+
+        if (self.batch_idx + 1) == self.num_batches:
+            self.update_lr()
+
+        return loss_summary
+    
+    def load_model(self, directory, epoch=None):
+        if not directory:
+            print(
+                'Note that load_model() is skipped as no pretrained model is given'
+            )
+            return
+
+        names = self.get_model_names()
+
+        # By default, the best model is loaded
+        model_file = 'model-best.pth.tar'
+
+        if epoch is not None:
+            model_file = 'model.pth.tar-' + str(epoch)
+
+        for name in names:
+            model_path = osp.join(directory, name, model_file)
+
+            if not osp.exists(model_path):
+                raise FileNotFoundError(
+                    'Model not found at "{}"'.format(model_path)
+                )
+
+            checkpoint = load_checkpoint(model_path)
+            state_dict = checkpoint['state_dict']
+            epoch = checkpoint['epoch']
+            
+            # Ignore fixed token vectors
+            if 'token_prefix' in state_dict:
+                del state_dict['token_prefix']
+            
+            if 'token_suffix' in state_dict:
+                del state_dict['token_suffix']
+
+            print(
+                'Loading weights to {} '
+                'from "{}" (epoch = {})'.format(name, model_path, epoch)
+            )
+            # set strict=False
+            self._models[name].load_state_dict(state_dict, strict=False)
+    
+    def run_epoch(self):
+        self.set_model_mode("train")
+        losses = MetricMeter()
+        batch_time = AverageMeter()
+        data_time = AverageMeter()
+        self.num_batches = len(self.train_loader_x)
+
+        end = time.time()
+        for self.batch_idx, batch in enumerate(self.train_loader_x):
+            data_time.update(time.time() - end)
+            loss_summary = self.forward_backward(batch)
+            batch_time.update(time.time() - end)
+            losses.update(loss_summary)
+
+            meet_freq = (self.batch_idx + 1) % self.cfg.TRAIN.PRINT_FREQ == 0
+            only_few_batches = self.num_batches < self.cfg.TRAIN.PRINT_FREQ
+            if meet_freq or only_few_batches:
+                nb_remain = 0
+                nb_remain += self.num_batches - self.batch_idx - 1
+                nb_remain += (
+                    self.max_epoch - self.epoch - 1
+                ) * self.num_batches
+                eta_seconds = batch_time.avg * nb_remain
+                eta = str(datetime.timedelta(seconds=int(eta_seconds)))
+
+                info = []
+                info += [f"epoch [{self.epoch + 1}/{self.max_epoch}]"]
+                info += [f"batch [{self.batch_idx + 1}/{self.num_batches}]"]
+                info += [f"time {batch_time.val:.3f} ({batch_time.avg:.3f})"]
+                info += [f"data {data_time.val:.3f} ({data_time.avg:.3f})"]
+                info += [f"{losses}"]
+                info += [f"lr {self.get_current_lr():.4e}"]
+                info += [f"eta {eta}"]
+                print(" ".join(info))
+
+            n_iter = self.epoch * self.num_batches + self.batch_idx
+            for name, meter in losses.meters.items():
+                self.write_scalar("train/" + name, meter.avg, n_iter)
+            self.write_scalar("train/lr", self.get_current_lr(), n_iter)
+
+            end = time.time()
+    
+    def parse_batch_test(self, batch):
+        input = batch["img"]
+        label = batch["label"]
+        domain = batch["domain"]
+
+        input = input.to(self.device)
+        label = label.to(self.device)
+        domain = domain.to(self.device)
+
+        return input, label, domain
+    
+    @torch.no_grad()
+    def test(self, split=None):
+        """A generic testing pipeline."""
+        self.set_model_mode("eval")
+        self.evaluator.reset()
+
+        if split is None:
+            split = self.cfg.TEST.SPLIT
+
+        if split == "val" and self.val_loader is not None:
+            data_loader = self.val_loader
+        else:
+            split = "test"  # in case val_loader is None
+            data_loader = self.test_loader
+
+        print(f"Evaluate on the *{split}* set")
+        eval_dict = {}
+        for batch_idx, batch in enumerate(tqdm(data_loader)):
+            input, label, domain = self.parse_batch_test(batch)
+            output = self.model_inference(input)
+            self.evaluator.process(output, label)
+
+            # for prv_domain in prv_domain_list:
+            prv_domain_index = [self.domain_list.index(prv_d) for prv_d in self.prv_domain_list if prv_d in self.domain_list]
+            prv_domain_mask = torch.isin(domain, torch.tensor(prv_domain_index).to(self.device))
+            # for del_domain in del_domain_list:
+            del_domain_index = [self.domain_list.index(del_d) for del_d in self.del_domain_list if del_d in self.domain_list]
+            del_domain_mask = torch.isin(domain, torch.tensor(del_domain_index).to(self.device))
+
+            eval_dict = compute_acc_for_df_eval(
+                eval_dict,
+                output,
+                label,
+                prv_domain_mask,
+                del_domain_mask,
+                domain,
+                self.domain_list,
+                self.device
+            ) 
+
+        results = self.evaluator.evaluate()
+        print("==========peservation or delete acc===============")
+        for name in ["prv", "del"]:
+            acc = eval_dict[f"correct_{name}"] / eval_dict[f"total_{name}"]
+            print(f"{name} : {acc:.2f}")
+        print("==============domain specific acc=================")
+        for domain_name in self.domain_list:
+            acc = eval_dict[f"correct_{domain_name}"] / eval_dict[f"total_{domain_name}"]
+            print(f"{domain_name} : {acc:.2f}")
+        print("===================================================")
+
+        for k, v in results.items():
+            tag = f"{split}/{k}"
+            self.write_scalar(tag, v, self.epoch)
+
+        return list(results.values())[0]
