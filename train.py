7d63066eebadc21ecb426c647a37893a726f83da
510yuta <yuta2m09s62.5488@outlook.jp>
2024-10-14 18:25:10 +0900

    vpt_with_domain classifierを追加

--- train.py
+++ train.py
@@ -43,6 +43,7 @@ import trainers.coop_cosembloss
 
 import trainers.vpt_with_dh
 import trainers.vpt_with_adapter
+import trainers.vpt_with_dc
 
 def print_args(args, cfg):
     print("***************")
