2e3e9245f741bb3813e5967030ae011ad42f422c
510yuta <yuta2m09s62.5488@outlook.jp>
2024-11-17 05:25:21 +0900

    shell script修正

--- scripts/independent-vlp-vladapter/domain_powerset.sh
+++ scripts/independent-vlp-vladapter/domain_powerset.sh
@@ -3,8 +3,11 @@ cd ../../
 gpu_id=$1
 vision_depth=$2
 text_depth=$3
+use_domain_cls_loss=$4
+use_nearest_neighbor_loss=$5
+is_divided=$6
 
-domains=("clipart" "painting" "real" "sketch")
+domains=("art" "clipart" "product" "real_world")
 for ((i = 1; i < 15; i++)); do
   # バイナリ数として各組み合わせを選択
   selected_domains=()
@@ -15,41 +18,7 @@ for ((i = 1; i < 15; i++)); do
   done
   # コマンドを実行
   selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
-  bash scripts/independent-vlp-vladapter/domain_forgetting.sh $gpu_id domainnet_mini_df 2 vit_b16_ep50 8 $vision_depth $text_depth $selected_domains_str
-  # bash scripts/vpt_with_dc/main.sh 0 domainnet_mini_df 1 vit_b16_ep50 8 9 $selected_domains_str
-  # echo "${selected_domains[@]}"
-done
-
-# domains=("art" "clipart" "product" "real_world")
-# for ((i = 1; i < 15; i++)); do
-#   # バイナリ数として各組み合わせを選択
-#   selected_domains=()
-#   for ((j = 0; j < 4; j++)); do
-#     if ((i & (1 << j))); then
-#       selected_domains+=("${domains[j]}")
-#     fi
-#   done
-#   # コマンドを実行
-#   selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
-#   bash scripts/independent-vlp-vladapter/domain_forgetting.sh $gpu_id office_home_df 1 vit_b16_ep50 8 $vision_depth $text_depth $selected_domains_str
-#   # echo "${selected_domains[@]}"
-# done
-
-# domains=("cartoon" "art_painting" "sketch" "photo")
-# for ((i = 1; i < 15; i++)); do
-#   # バイナリ数として各組み合わせを選択
-#   selected_domains=()
-#   for ((j = 0; j < 4; j++)); do
-#     if ((i & (1 << j))); then
-#       selected_domains+=("${domains[j]}")
-#     fi
-#   done
-#   # コマンドを実行
-#   selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
-#   bash scripts/independent-vlp-vladapter/domain_forgetting.sh $gpu_id pacs_df 1 vit_b16_ep50 8 $vision_depth $text_depth $selected_domains_str
-#   # echo "${selected_domains[@]}"
-# done
-  bash scripts/independent-vlp-vladapter/domain_forgetting.sh $gpu_id office_home_df 2 vit_b16_ep50 8 $vision_depth $text_depth $selected_domains_str
+  bash scripts/independent-vlp-vladapter/domain_forgetting.sh $gpu_id office_home_df 1 vit_b16_ep50 8 $vision_depth $text_depth $use_domain_cls_loss $use_nearest_neighbor_loss $is_divided $selected_domains_str
   # echo "${selected_domains[@]}"
 done
 
@@ -64,6 +33,22 @@ for ((i = 1; i < 15; i++)); do
   done
   # コマンドを実行
   selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
-  bash scripts/independent-vlp-vladapter/domain_forgetting.sh $gpu_id pacs_df 2 vit_b16_ep50 8 $vision_depth $text_depth $selected_domains_str
+  bash scripts/independent-vlp-vladapter/domain_forgetting.sh $gpu_id pacs_df 1 vit_b16_ep50 8 $vision_depth $text_depth $use_domain_cls_loss $use_nearest_neighbor_loss $is_divided $selected_domains_str
+  # echo "${selected_domains[@]}"
+done
+
+domains=("clipart" "painting" "real" "sketch")
+for ((i = 1; i < 15; i++)); do
+  # バイナリ数として各組み合わせを選択
+  selected_domains=()
+  for ((j = 0; j < 4; j++)); do
+    if ((i & (1 << j))); then
+      selected_domains+=("${domains[j]}")
+    fi
+  done
+  # コマンドを実行
+  selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
+  bash scripts/independent-vlp-vladapter/domain_forgetting.sh $gpu_id domainnet_mini_df 1 vit_b16_ep50 8 $vision_depth $text_depth $use_domain_cls_loss $use_nearest_neighbor_loss $is_divided $selected_domains_str
+  # bash scripts/vpt_with_dc/main.sh 0 domainnet_mini_df 1 vit_b16_ep50 8 9 $selected_domains_str
   # echo "${selected_domains[@]}"
 done
