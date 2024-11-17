e6f1e931ff8936fcdddb1a15a8aae72603719ad1
510yuta <yuta2m09s62.5488@outlook.jp>
2024-11-17 19:39:20 +0900

    vpt用のものをさくせい

--- scripts/vpt/domain_powerset.sh
+++ scripts/vpt/domain_powerset.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd ../../
+use_domain_cls_loss=$1
+use_nearest_neighbor_loss=$2
+is_divided=$3
 
 domains=("clipart" "painting" "real" "sketch")
-for ((i = 10; i < 15; i++)); do
+for ((i = 1; i < 15; i++)); do
   # バイナリ数として各組み合わせを選択
   selected_domains=()
   for ((j = 0; j < 4; j++)); do
@@ -12,7 +15,7 @@ for ((i = 10; i < 15; i++)); do
   done
   # コマンドを実行
   selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
-  bash scripts/vpt/main.sh 0 domainnet_mini_df 1 vit_b16_ep50 8 9 $selected_domains_str
+  bash scripts/vpt/main.sh 1 domainnet_mini_df 1 vit_b16_ep50 8 9 $use_domain_cls_loss $use_nearest_neighbor_loss $is_divided $selected_domains_str
   # bash scripts/vpt_with_dc/main.sh 0 domainnet_mini_df 1 vit_b16_ep50 8 9 $selected_domains_str
   # echo "${selected_domains[@]}"
 done
@@ -34,46 +37,32 @@ done
 #   # echo "${selected_domains[@]}"
 # done
 
-# domains=("cartoon" "art_painting" "sketch" "photo")
-# for ((i = 3; i < 15; i++)); do
-#   # バイナリ数として各組み合わせを選択
-#   selected_domains=()
-#   for ((j = 0; j < 4; j++)); do
-#     if ((i & (1 << j))); then
-#       selected_domains+=("${domains[j]}")
-#     fi
-#   done
-#   # コマンドを実行
-#   selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
-#   bash scripts/vpt/main.sh 3 pacs_df 1 vit_b16_ep50 8 9 $selected_domains_str
-#   # echo "${selected_domains[@]}"
-# done
+domains=("cartoon" "art_painting" "sketch" "photo")
+for ((i = 1; i < 15; i++)); do
+  # バイナリ数として各組み合わせを選択
+  selected_domains=()
+  for ((j = 0; j < 4; j++)); do
+    if ((i & (1 << j))); then
+      selected_domains+=("${domains[j]}")
+    fi
+  done
+  # コマンドを実行
+  selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
+  bash scripts/vpt/main.sh 1 pacs_df 1 vit_b16_ep50 8 9 $use_domain_cls_loss $use_nearest_neighbor_loss $is_divided $selected_domains_str
+  # echo "${selected_domains[@]}"
+done
 
-# domains=("art" "clipart" "product" "real_world")
-# for ((i = 3; i < 15; i++)); do
-#   # バイナリ数として各組み合わせを選択
-#   selected_domains=()
-#   for ((j = 0; j < 4; j++)); do
-#     if ((i & (1 << j))); then
-#       selected_domains+=("${domains[j]}")
-#     fi
-#   done
-#   # コマンドを実行
-#   selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
-#   bash scripts/vpt/main.sh 0 pacs_df 0 vit_b16_ep50 8 9 $selected_domains_str
-#   # echo "${selected_domains[@]}"
-# done
-# domains=("art" "clipart" "product" "real_world")
-# for ((i = 1; i < 15; i++)); do
-#   # バイナリ数として各組み合わせを選択
-#   selected_domains=()
-#   for ((j = 0; j < 4; j++)); do
-#     if ((i & (1 << j))); then
-#       selected_domains+=("${domains[j]}")
-#     fi
-#   done
-#   # コマンドを実行
-#   selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
-#   bash scripts/vpt/main.sh 0 office_home_df 0 vit_b16_ep50 8 9 $selected_domains_str
-#   # echo "${selected_domains[@]}"
-# done
+domains=("art" "clipart" "product" "real_world")
+for ((i = 1; i < 15; i++)); do
+  # バイナリ数として各組み合わせを選択
+  selected_domains=()
+  for ((j = 0; j < 4; j++)); do
+    if ((i & (1 << j))); then
+      selected_domains+=("${domains[j]}")
+    fi
+  done
+  # コマンドを実行
+  selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
+  bash scripts/vpt/main.sh 1 office_home_df 1 vit_b16_ep50 8 9 $use_domain_cls_loss $use_nearest_neighbor_loss $is_divided $selected_domains_str
+  # echo "${selected_domains[@]}"
+done
