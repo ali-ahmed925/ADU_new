e8f8f02c492de814e155ea01c73e87cc2886155e
510yuta <yuta2m09s62.5488@outlook.jp>
2024-11-21 23:55:00 +0900

    一旦

--- scripts/vpt_with_adapter/domain_powerset.sh
+++ scripts/vpt_with_adapter/domain_powerset.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 cd ../../
-domains=("cartoon" "art_painting" "sketch" "photo")
-for ((i = 1; i < 15; i++)); do
-  # バイナリ数として各組み合わせを選択
-  selected_domains=()
-  for ((j = 0; j < 4; j++)); do
-    if ((i & (1 << j))); then
-      selected_domains+=("${domains[j]}")
-    fi
-  done
-  # コマンドを実行
-  selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
-  bash scripts/vpt_with_adapter/main.sh 3 pacs_df 1 vit_b16_ep50 8 9 $selected_domains_str
-  # echo "${selected_domains[@]}"
-done
+gpu_id=$1
+
 domains=("art" "clipart" "product" "real_world")
 for ((i = 1; i < 15; i++)); do
   # バイナリ数として各組み合わせを選択
@@ -25,6 +13,70 @@ for ((i = 1; i < 15; i++)); do
   done
   # コマンドを実行
   selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
-  bash scripts/vpt_with_adapter/main.sh 3 office_home_df 1 vit_b16_ep50 8 9 $selected_domains_str
+  bash scripts/coop_with_adapter/main.sh $gpu_id office_home_df vit_b16_ep50 end 8 False $selected_domains_str
+  bash scripts/vpt_with_adapter/main.sh $gpu_id office_home_df 1 vit_b16_ep50 8 9 $selected_domains_str
   # echo "${selected_domains[@]}"
 done
+
+domains=("cartoon" "art_painting" "sketch" "photo")
+for ((i = 1; i < 15; i++)); do
+  # バイナリ数として各組み合わせを選択
+  selected_domains=()
+  for ((j = 0; j < 4; j++)); do
+    if ((i & (1 << j))); then
+      selected_domains+=("${domains[j]}")
+    fi
+  done
+  # コマンドを実行
+  selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
+  bash scripts/coop_with_adapter/main.sh $gpu_id pacs_df vit_b16_ep50 end 8 False $selected_domains_str
+  bash scripts/vpt_with_adapter/main.sh $gpu_id pacs_df 1 vit_b16_ep50 8 9 $selected_domains_str
+done
+
+domains=("clipart" "painting" "real" "sketch")
+for ((i = 1; i < 15; i++)); do
+  # バイナリ数として各組み合わせを選択
+  selected_domains=()
+  for ((j = 0; j < 4; j++)); do
+    if ((i & (1 << j))); then
+      selected_domains+=("${domains[j]}")
+    fi
+  done
+  # コマンドを実行
+  selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
+  bash scripts/coop_with_adapter/main.sh $gpu_id domainnet_mini_df vit_b16_ep50 end 8 False $selected_domains_str
+  bash scripts/vpt_with_adapter/main.sh $gpu_id domainnet_mini_df 1 vit_b16_ep50 8 9 $selected_domains_str
+  # bash scripts/vpt_with_dc/main.sh 0 domainnet_mini_df 1 vit_b16_ep50 8 9 $selected_domains_str
+  # echo "${selected_domains[@]}"
+done
+
+
+
+# domains=("cartoon" "art_painting" "sketch" "photo")
+# for ((i = 1; i < 15; i++)); do
+#   # バイナリ数として各組み合わせを選択
+#   selected_domains=()
+#   for ((j = 0; j < 4; j++)); do
+#     if ((i & (1 << j))); then
+#       selected_domains+=("${domains[j]}")
+#     fi
+#   done
+#   # コマンドを実行
+#   selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
+#   bash scripts/vpt_with_adapter/main.sh 3 pacs_df 1 vit_b16_ep50 8 9 $selected_domains_str
+#   # echo "${selected_domains[@]}"
+# done
+# domains=("art" "clipart" "product" "real_world")
+# for ((i = 1; i < 15; i++)); do
+#   # バイナリ数として各組み合わせを選択
+#   selected_domains=()
+#   for ((j = 0; j < 4; j++)); do
+#     if ((i & (1 << j))); then
+#       selected_domains+=("${domains[j]}")
+#     fi
+#   done
+#   # コマンドを実行
+#   selected_domains_str=$(IFS=" "; echo "${selected_domains[*]}")
+#   bash scripts/vpt_with_adapter/main.sh 3 office_home_df 1 vit_b16_ep50 8 9 $selected_domains_str
+#   # echo "${selected_domains[@]}"
+# done
--- scripts/zsclip/zeroshot.sh
+++ scripts/zsclip/zeroshot.sh
@@ -8,13 +8,22 @@ TRAINER=ZeroshotCLIP
 DATASET=$1
 CFG=$2  # rn50, rn101, vit_b32 or vit_b16
 gpuid=$3
+DOMAIN_LIST=("${@:4}")
+DOMAIN_SEC=$(IFS=-; echo "${DOMAIN_LIST[*]}")
+DOMAIN_COUNT=${#DOMAIN_LIST[@]}
+TODAY=$(date +"%Y%m%d_%H%M%S")
+SHOTS=16
+
+DIR=/nas/data/gotoyuta/Result_Domain_Forgetting/${DATASET}/${TRAINER}/FORGET_DOMAIN${DOMAIN_COUNT}/${DOMAIN_SEC}/${TODAY}
+CSV_FILE_PATH=/nas/data/gotoyuta/Result_Domain_Forgetting/${DATASET}/${TRAINER}/FORGET_DOMAIN${DOMAIN_COUNT}/zero-shot.csv
 
 CUDA_VISIBLE_DEVICES=${gpuid} python train.py \
     --root ${DATA} \
     --trainer ${TRAINER} \
     --dataset-config-file configs/datasets/${DATASET}.yaml \
     --config-file configs/trainers/CoOp/${CFG}.yaml \
-    --output-dir /nas/data/gotoyuta/Result_Domain_Forgetting/${DATASET}/${TRAINER}/${CFG}/${DATASET} \
+    --forget_domains "${DOMAIN_LIST[@]}" \
+    --output-dir ${DIR} \
+    --csv_file_path ${CSV_FILE_PATH} \
     --eval-only \
-    --no_forget \
-    --forget_domain none
+    
