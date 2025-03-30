0ce0a34b8f142156382c775f40dd2a6d532c9007
510yuta <yuta2m09s62.5488@outlook.jp>
2025-03-31 05:34:50 +0900

    一旦

--- scripts_loop/clipfit/domain_powerset.sh
+++ scripts_loop/clipfit/domain_powerset.sh
@@ -3,8 +3,9 @@ gpuid=${1}
 expname=Baseline
 
 cd ../../
-
-# bash scripts_loop/clipfit/domain_forgetting.sh $gpuid imagenet_df 1 vit_b16_ep50 16 Baseline 1 
+for shots in 2 4 8;do
+bash scripts_loop/clipfit/domain_forgetting.sh $gpuid imagenet_df 1 vit_b16_ep50 $shots Baseline 1 
+done
 # bash scripts_loop/clipfit/domain_forgetting.sh $gpuid office_home_df 1 vit_b16_ep50 16 Baseline 1 
-bash scripts_loop/clipfit/domain_forgetting.sh $gpuid domainnet_mini_df 1 vit_b16_ep50 2 Baseline 1 
+# bash scripts_loop/clipfit/domain_forgetting.sh $gpuid domainnet_mini_df 1 vit_b16_ep50 2 Baseline 1 
 # bash scripts_loop/clipfit/domain_forgetting.sh $gpuid imagenet_df 1 vit_b16_ep50 16 Baseline 1 
